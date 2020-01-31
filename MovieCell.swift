9ff1168a184bff0f07fc09bab7b8ea11cc858924
Adam Lei <billparker149@gmail.com>
2020-01-30 22:24:27 -0800

    Still has some problem need help

---
+++ MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Adam on 1/30/20.
+//  Copyright © 2020 Adam. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
